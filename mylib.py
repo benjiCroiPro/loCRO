e62d95878678e01397414d7d601b1839b9831dd4
Benji Cohen <benji.cohen@iprospect.com>
2018-03-13 15:48:11 +0000

    Updated generic template to use css file instead of scss, commented the crap outta mylib and scrape python scripts, tool now contains 100% less Sass but 23% more sass

--- mylib.py
+++ mylib.py
@@ -1,129 +1,127 @@
+# import relevant packages for use in this script
 from urllib.parse import urlsplit
 from os.path import basename
 import os, errno, re, shutil, urllib.request
 
+# define scrape and save function, arguments are link, directory, file name and original url
 def scrape_and_save(link, directory, file_name, original_url):
+	# stip trailing slash and store original link and directory variables 
 	orig_link = re.sub(r'\/$', '', link)
 	orig_dir = re.sub(r'\/$', '', directory)
+	# if the filename is index.html, set the filepath to original_directory
 	if file_name == 'index.html':
 		link_file_path = orig_dir + '/' + file_name
 	else:
+		# else, get the file_name from the file_path_generator function (pass file_name from original function call to this function)
 		f_p = file_path_generator(file_name)
+		# if filepath doesn't start with '/', add a '/'
 		if f_p[0] != '/':
 			f_p = '/' + f_p
-
-		directory = orig_dir + '/' + f_p
+		# set new directory variable for file parent folder 
+		directory = orig_dir + f_p
+		# set link_file_path variable for filepath directly to file, uses url2name function (pass file_name from original function call to this function)
 		link_file_path = directory + '/' + url2name(file_name)
+		# if original link is in the asset
 		if original_url in link:
+			# set link variable to original_url + link (using regex to remove orinal url from link. This ensure consistent urls) 
 			link  = original_url+'/'+re.sub(r"^/", '', link.replace(original_url, ''))
+		# if first character of link is '/' and second character isn't 
 		elif link[0] == '/' and link[1] != '/':
+			# set link variable to original_url + link (using regex to remove orinal url from link. This ensure consistent urls) 
 			link  = original_url+'/'+re.sub(r"^/", '', link.replace(original_url, ''))
+		# if first character and second character of link is '/' 
 		elif link[0] == '/' and link[1] == '/':
+			# asset is 3rd party, add 'https:' to start of string
 			link  = 'https:'+link
 
+	# try and make new directory
 	try:
 		os.makedirs(directory)
+	# raise error if error not errno.EExist
 	except OSError as e:
 		if e.errno != errno.EEXIST:
 			raise
 
+	# try and download asset
 	try:
 		urllib.request.urlretrieve(link, link_file_path)
+	# raise errors
 	except Exception as e:
 		raise e
 
+	# if filename isn't index.html
 	if file_name != 'index.html':
+		# log message to console
 		print ("Saving ingredient: " + link)
+		# try opening index.html file with read permissions
 		try:
 			index = open(orig_dir+'/index.html', 'r')
-
+		# raise errors
 		except Exception as e:
 			raise e
-
+		# store file content and replace original asset link with local equivalent
 		new_content = index.read().replace(orig_link, link_file_path)
+		# close index file
 		index.close()
+		# reopen with write permissions
 		index = open(orig_dir+'/index.html', 'w')
+		# replace file content with new version
 		index.write(new_content)
+		# close file
 		index.close()
+	#  return True end of function
 	return True
 
+# finds and returns file name from url
 def url2name(url):
 	return basename(urlsplit(url)[2])
 
+# returns filepath without filename
 def file_path_generator(url):
 	url = urlsplit(url)[2]
 	file_name = basename(url)
 	return url.replace(file_name, '')
 
+# function to generate dev enviroment from template folder
 def generate_dev_enviroment(config):
+	# set variables based on config file
 	client_directory = config["client_directory"]
 	test_id = config["test_id"]
 	test_directory = client_directory + test_id
 	site_directory = test_directory + '/site'
-	code_directory = test_directory + '/code'
 	code_template = config["enviroment_template"]
 	
-	# insert locro tag
+	# try opening local site index.html with read permissions 
 	try:
 		index = open(site_directory+'/index.html', 'r')
 	except Exception as e:
 		raise e
 
+	# update index content to include locro script tag and store in variable
 	new_content = index.read().replace('</body>', '\t<script src="locro/locro.js"></script>\n</body>')
+	# close file
 	index.close()
+	
+	# open local site index.html with write permissions 
 	index = open(site_directory+'/index.html', 'w')
+	# replace index content with updated content
 	index.write(new_content)
+	# close file
 	index.close()
 
+	# copy code_template folder (as defined in config.json) to test directory
 	shutil.copytree(code_template, test_directory + '/code')
 
-	# # create locro directory in site
-	# try:
-	# 	os.makedirs(site_directory + '/locro')
-	# except OSError as e:
-	# 	if e.errno != errno.EEXIST:
-	# 		raise
-	# # create locro tag
-	# try:
-	# 	copy('locro.js', site_directory + '/locro')
-	# except Exception as e:
-	# 	raise e
-
-	# try:
-	# 	os.makedirs(code_directory)
-	# except OSError as e:
-	# 	if e.errno != errno.EEXIST:
-	# 		raise
-
-	# try:
-	# 	os.makedirs(code_directory + '/build')
-	# except OSError as e:
-	# 	if e.errno != errno.EEXIST:
-	# 		raise
-
-	# # JS Template
-	# try:
-	# 	v1_js = open(code_directory+'/build/v1.js', 'w')
-	# except Exception as e:
-	# 	raise e
-
-	# v1_js.write('// Variation JS goes here\n')
-	# v1_js.close()
-
-	# # CSS Template
-	# try:
-	# 	v1_css = open(code_directory+'/build/v1.scss', 'w')
-	# except Exception as e:
-	# 	raise e
-
-	# v1_css.write('/* Variation sass goes here */\n')
-	# v1_css.close()
-	
-	# # Gulp Template/Boilerplate
-	# try:
-	# 	copy('gulpfile.js', code_directory)
-	# except Exception as e:
-	# 	raise e
-	# # TODO
-	# 	# CSS Preprocessor
-	# 	# JS Transpiler
+	# try create locro directory in local site folder
+	try:
+		os.makedirs(site_directory + '/locro')
+	# raise error if error not errno.EEXIST
+	except OSError as e:
+		if e.errno != errno.EEXIST:
+			raise
+	# try copying locro.js file to local site locro folder
+	try:
+		copy('locro.js', site_directory + '/locro')
+	# raise error
+	except Exception as e:
+		raise e
--- scrape.py
+++ scrape.py
@@ -1,13 +1,20 @@
+# import relevant packages for use in this script
 from bs4 import BeautifulSoup
 from pathlib import Path
 import mylib, json, requests, re
 
+# set config file path
 config_file = Path("config.json")
+# check if config file exists
 if config_file.is_file():
+	# success message
 	print ("Config file exists")
 
+	# open config file
 	with open('config.json', 'r') as f:
+		# relevant messaging
 		print ("Checking config file")
+		# set variables based on config settings
 		config = json.load(f)
 		original_url = config['url']
 		client_directory = config["client_directory"]
@@ -16,49 +23,70 @@ if config_file.is_file():
 		site_directory = test_directory + '/site'
 		code_directory = test_directory + '/code'
 
+		# get the requested url
 		r  = requests.get(original_url)
 
+		# store html from url in variable
 		data = r.text
 
+		# parse the HTML with BeautifulSoup package
 		print ("Making soup")
 		soup = BeautifulSoup(data, "html.parser")
 
+		# pass the requested url data over to the scrape and save function in the mylib package
 		print ("Saving the recipe")
 		mylib.scrape_and_save(original_url, site_directory, 'index.html', original_url)
 
+		# store all link tags in variable
 		print ("Getting the ingredients")
 		asset_tags = soup.find_all("link")
 
+		# loop through all script tags and push to asset_tags variable (array)
 		for a in soup.find_all("script"):
 			asset_tags.append(a)
 
+		# loop through all img tags and push to asset_tags variable (array)
 		for a in soup.find_all("img"):
 			asset_tags.append(a)
 
+		# loop through all div tags and push to asset_tags variable (array)
 		for a in soup.find_all("div"):
 			asset_tags.append(a)
 
+		# loop through all stored tags in asset_tags variable
 		for asset in asset_tags:
+			# set href variable to false because setting it in the else statement caused errors
 			href = False
+			# if asset is a stylesheet
 			if asset.has_attr('href') and asset.has_attr("rel"):
 				if "stylesheet" in asset.get("rel"):
 					href = asset.get('href')
+			# if asset has src attribute
 			elif asset.has_attr('src'):
 				href = asset.get('src')
+			# if asset has data-original attribute
 			elif asset.has_attr('data-original'):
 				href = asset.get('data-original')
+			# if asset has data-src attribute
 			elif asset.has_attr('data-src'):
 				href = asset.get('data-src')
+			# if asset has data-image-lg attribute
 			elif asset.has_attr('data-image-lg'):
 				href = asset.get('data-image-lg')
 
+			# if href not equal to false
 			if href != False:
+				# if url in href (doing this to detect 'url("")' in attributes)
 				if "url" in href:
+					# regex to strip unwanted characters
 					href = re.sub(r"url|\(|\)|\"|\'", "", href)
+					# print asset href and pass details onto scrape_and_save function
 					print ("Found ingredient: "+ href)
 					mylib.scrape_and_save(href, site_directory, href, original_url)
 				else:
+					# print asset href and pass details onto scrape_and_save function
 					print ("Found ingredient: "+ href)
 					mylib.scrape_and_save(href, site_directory, href, original_url)
-
+					
+		# Once done pass config file onto generae_dev_enviroment (function found in mylib file)
 		mylib.generate_dev_enviroment(config)
